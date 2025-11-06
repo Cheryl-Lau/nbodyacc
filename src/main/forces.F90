d76b0e771f61b589f19bd3b2a1cb5aea65529caf
Cheryl Lau <lausiching@gmail.com>
2025-11-07 04:41:04 +0800

    rm softening; changed dt settings

--- src/main/forces.F90
+++ src/main/forces.F90
@@ -5,7 +5,7 @@ module force
  public :: compute_forces
  public :: read_infile_force,write_infile_force
 
- integer, public :: iextforce = 3
+ integer, public :: iextforce = 1
 
  integer, parameter, public :: &
    iext_plummer  = 1, &
@@ -59,32 +59,21 @@ subroutine self_grav(nptmass,xyzhm_ptmass,fxyz_ptmass,poten_ptmass)
  real,    intent(in)    :: xyzhm_ptmass(:,:)
  real,    intent(inout) :: fxyz_ptmass(:,:),poten_ptmass(:)
  integer :: i,j
- real    :: r_ij(3),fsum(3),f_ij(3),absr,mi,mj,phi,phisum,hi,hj,hsoft 
+ real    :: r_ij(3),fsum(3),f_ij(3),absr,mi,mj,phi,phisum
 
  do i = 1,nptmass
     fsum   = 0.d0 
     phisum = 0.d0 
     mi     = xyzhm_ptmass(5,i)
-    hi     = xyzhm_ptmass(4,i)
     over_neigh: do j = 1,nptmass
         if (i /= j) then 
             mj    = xyzhm_ptmass(5,j)
             r_ij  = xyzhm_ptmass(1:3,i) - xyzhm_ptmass(1:3,j)
             absr  = sqrt(dot_product(r_ij,r_ij))
-            hj    = xyzhm_ptmass(4,j)
-            hsoft = 2.d0*max(hi,hj)
-
-            softening: if (absr < hsoft) then 
-               f_ij = -mj*r_ij/(absr+hsoft)**3
-               fsum = fsum + f_ij 
-               phi  = -mi/(absr+hsoft)
-               phisum = phisum + phi 
-            else 
-               f_ij = -mj*r_ij/absr**3    ! per mass(i); G=1 in code units 
-               fsum = fsum + f_ij 
-               phi  = -mi/absr
-               phisum = phisum + phi 
-            endif softening 
+            f_ij = -mj*r_ij/absr**3    ! per mass(i); G=1 in code units 
+            fsum = fsum + f_ij 
+            phi  = -mi/absr
+            phisum = phisum + phi 
         endif 
     enddo over_neigh 
     fxyz_ptmass(1:3,i) = fxyz_ptmass(1:3,i) + fsum
--- src/main/timestep.f90
+++ src/main/timestep.f90
@@ -5,11 +5,11 @@ module timestep
  public :: constrain_dt 
  public :: read_infile_timestep,write_infile_timestep
  
- integer, public :: nout    = 100     ! write dump every <nout> dtmax 
- integer, public :: maxdump = 100     ! max number of dumpfiles 
- real,    public :: dtmax   = 1.d-6   ! sim max timestep 
+ integer, public :: nout    = 10      ! write dump every <nout> dtmax 
+ integer, public :: maxdump = 300     ! max number of dumpfiles 
+ real,    public :: dtmax   = 1.d-5   ! sim max timestep 
  real,    public :: t_init  = 0.d0    ! sim start time 
- real,    public :: t_end   = 1.d-2   ! sim end time 
+ real,    public :: t_end   = 3.d-2   ! sim end time 
 
  integer, public :: nbinmax  = 20
  real,    public :: alpha_dt = 1.d-4    ! timestep constraint param 
